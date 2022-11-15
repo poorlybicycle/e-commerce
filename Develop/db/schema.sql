a95a6e64a880c78b542f87255e592781553791d9
Mime Jr <petravidovic8@gmail.com>
2022-11-15 13:56:29 -0500

    edit routes and add try/catch

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -5,30 +5,4 @@ DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
 USE ecommerce_db;
 
-CREATE TABLE product (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    --string??
-    product_name STRING INT NOT NULL,
-    --how to validate that value is a decimal
-    price DECIMAL NOT NULL,
-    stock INT NOT NULL DEFAULT(10), 
-    --not sure about this part
-    category_id INT 
-    REFERENCES category(id)
-);
-
-CREATE TABLE tag (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    --string??
-    tag_name STRING NOT NULL,
-);
-
-CREATE TABLE ProductTag (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    --string??
-    product_id INT NOT NULL
-    REFERENCES product(id),
-    tag_id INT 
-    REFERENCES tag(id)
-);
 
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,12 +3,36 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+try {
+  var categoryData = await Category.findAll({
+    include: [Product]
+  })
+  if(!categoryData){
+    res.json({"message": "There is no data in categories"})
+  }
+  res.json(categoryData)
+} catch (error) {
+  console.log("In the get all categories route")
+  console.log(error)
+}
+
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+try {
+  var categoryData = await Category.findByPk(req.params.id,{
+    include: [Product]
+  })
+  res.json(categoryData)
+} catch (error) {
+  console.log(error)
+}
+
+
+
   // find one category by its `id` value
   // be sure to include its associated Products
 });
