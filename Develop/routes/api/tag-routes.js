2c18c5aeb859c62eb8378307aebbf89f36fa42fe
Mime Jr <petravidovic8@gmail.com>
2022-11-16 22:25:50 -0500

    finish tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -17,20 +17,46 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+  try {
+    let tag = await Tag.findByPk(req.params.id, {
+      include: [Product]
+    })
+    res.json(tag)
+  } catch(error) {
+    res.json(error)
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
 router.post('/', async (req, res) => {
+  try {
+    let newTag = await Tag.create(req.body)
+    res.json(newTag)
+  } catch(error) {
+    res.json(error)
+  }
   // create a new tag
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    let updateTag = await Tag.update(req.body, {where:{id:req.params.id}})
+    res.json(updateTag)
+  } catch(error) {
+    res.json(error)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    let deleteTag = await Tag.destroy({where:{id:req.params.id}})
+    res.json(deleteTag)
+  } catch (error) {
+    res.json(error)
+  }
 });
 
 module.exports = router;
